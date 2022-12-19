4f1008f98e1a5eab555e197c7a23ded5f37b7f22
Henry Teng <henry_teng@realtek.com>
2022-12-19 16:01:40 +0800

    [abr_thread] fix folder name. [abr_algorithm] init.

--- auto_upload.py
+++ auto_upload.py
@@ -1,5 +1,7 @@
 import os
 
-os.system('rm -r dist')
-os.system('python setup.py sdist')
-os.system('twine upload dist/*')
+os.system('rmdir /s /q dist')
+# os.system('python setup.py sdist')
+os.system('py -m build')
+# os.system('py -m twine upload --repository testpypi dist/*') # 測試
+os.system('twine upload dist/*') # 正式發布
