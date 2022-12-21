be7bd924a0ebd0cdd5aaac6088f62432c803f30f
Henry Teng <henry_teng@realtek.com>
2022-12-21 13:00:21 +0800

    [auto upload] update script.

--- auto_upload.py
+++ auto_upload.py
@@ -1,7 +1,11 @@
 import os
 
-os.system('rmdir /s /q dist')
-# os.system('python setup.py sdist')
-os.system('py -m build')
-# os.system('py -m twine upload --repository testpypi dist/*') # 測試
-os.system('twine upload dist/*') # 正式發布
+def main():
+    os.system('rmdir /s /q dist')
+    # os.system('python setup.py sdist')
+    os.system('py -m build')
+    # os.system('py -m twine upload --repository testpypi dist/*') # 測試
+    os.system('twine upload dist/*') # 正式發布
+
+if __name__ == "__main__":
+    main()
